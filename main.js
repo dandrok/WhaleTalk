18bb3d7cff00626ed8a7c4a25f2e9e971eda8fa7
Patryk <46764643+paT1ku@users.noreply.github.com>
2020-08-03 00:25:33 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+let input = 'turpentine and turtles'; //write what you wanna trsnslate to whale language
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+
+for (let i = 0; i < input.length; i++) {
+  //console.log('i is '+ i);
+  for (let j = 0; j < vowels.length; j++) {
+  //console.log('j is '+ j);
+    if(input[i] === vowels[j]) {
+      if (input[i] === 'e') {
+        resultArray.push('ee');
+      }
+      else if (input[i] === 'u') {
+        resultArray.push('uu');
+      }
+      else {
+        resultArray.push(input[i]);
+      }
+    }
+  } 
+}
+
+console.log(resultArray.join('').toUpperCase()); //translate input to whale language
